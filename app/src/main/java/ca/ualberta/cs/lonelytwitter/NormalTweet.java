8c4ead0d385513d3b89b0b857cc91f19e6d91bda
Yipeng Zhou <yipeng6@cs.ualberta.ca>
2017-01-19 18:54:39 -0700

    lab2_end

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by yipeng6 on 1/19/17.
+ */
+
+public class NormalTweet extends Tweet {
+    public NormalTweet(String message) {
+        super(message);
+    }
+
+    public NormalTweet(Date date, String message) {
+        super(date, message);
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by yipeng6 on 1/19/17.
+ */
+
+public class TweetTooLongException extends Exception{
+
+}
